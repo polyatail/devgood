5af3b1d01ba79b28f815e85710a12cb4b34c3acb
seema <seema.hari87@gmail.com>
2013-11-16 05:35:43 -0800

    project details

--- devgoodapp/views.py
+++ devgoodapp/views.py
@@ -32,10 +32,10 @@ def dev_dashboard(request):
         return HttpResponse("You need to login")
 
 def project_detail(request,requested_project_id):
-    project = Project.objects.filter(project__id__exact=requested_project_id)
-    return HttpResponse("requested project id is  %s." % project)
-#    return render_to_response(
-#	"project_detail.html", 
-#	{"project": project}
-#    )
+    project = Project.objects.filter(id=1)
+#    return HttpResponse("requested project id is  %s." % project)
+    return render_to_response(
+	"project_detail.html", 
+	{"project": project}
+    )
     
